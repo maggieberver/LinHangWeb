b30c274310cb9a89288f1c9af7bfbc6f83cf19e6
damian <damian@LAB2-MAESTRO>
2026-05-12 10:35:54 -0600

    Agregar archivos de proyecto.

---
+++ Segunda4F/Data/DirectorioDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Segunda4F.Data
+{
+    public class DirectorioDBContext : DbContext
+    {
+        public DirectorioDBContext(DbContextOptions<DirectorioDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
---
+++ Segunda4F/Repositorios/RepositorioPersonas.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Segunda4F.Data;
+
+namespace Segunda4F.Repositorios
+{
+    public class RepositorioPersonas : IRepositorioPersonas
+    {
+        readonly DirectorioDBContext _context;
+        public RepositorioPersonas(DirectorioDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task ActualizarPersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AgregarPersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EliminarPersona(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Persona> ObtenerPersonaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Persona>> ObtenerPersonas()
+        {
+            return await _context.Personas.ToListAsync();
+        }
+    }
+}
